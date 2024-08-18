8ac6a273294abdf82ed9cd4ad7c9e4204c23a773
seinnlet <57383173+seinnlet@users.noreply.github.com>
2024-08-19 00:05:58 +0900

    update createFirstMessage

--- db_chat.php
+++ db_chat.php
@@ -11,32 +11,51 @@
 		return $userList;
 	}
 
+	// function getRoomList() {
+	// 	$conn = dbConnect();
+
+	// }
+
+	function createFirstMessage($sendToID, $message) {
+		$conn = dbConnect();
+
+		$sql = 'INSERT INTO rooms () VALUES ();';
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+
+		$roomId = $conn->lastInsertId();
+
+		$sql = 'INSERT INTO messages (`text`, `sender_id`, `room_id`) VALUES (:message, :sender_id, :room_id);';
+		$stmt = $conn->prepare($sql);
+		$stmt->bindValue(':message', $message, PDO::PARAM_STR);
+		$stmt->bindValue(':sender_id', (int)$_COOKIE['user_id'], PDO::PARAM_STR);
+		$stmt->bindValue(':room_id',(int)$roomId, PDO::PARAM_INT);
+		$stmt->execute();
+
+		$sql = 'INSERT INTO participants (`room_id`, `user_id`) VALUES (:room_id, :user_id);';
+		$stmt = $conn->prepare($sql);
+		$stmt->bindValue(':room_id',(int)$roomId, PDO::PARAM_INT);
+		$stmt->bindValue(':user_id', (int)$_COOKIE['user_id'], PDO::PARAM_STR);
+		$stmt->execute();
+		
+		$sql = 'INSERT INTO participants (`room_id`, `user_id`) VALUES (:room_id, :user_id);';
+		$stmt = $conn->prepare($sql);
+		$stmt->bindValue(':room_id',(int)$roomId, PDO::PARAM_INT);
+		$stmt->bindValue(':user_id',(int)$sendToID, PDO::PARAM_STR);
+		$stmt->execute();
+
+		return $roomId;
+	}
+
 	if (isset($_POST['btnSend'])) {
 		
 		$sendToID = $_POST['slUserList'];
 		$message = htmlspecialchars($_POST['taMessage']);
 		
-		$roomId = createMessage($sendToID, $message);
+		$roomId = createFirstMessage($sendToID, $message);
 		header("Location: chat.php?r_id=".$roomId);
 	}
 
-	function createMessage($sendToID, $message) {
-		$conn = dbConnect();
 
-		$sql = 'INSERT INTO rooms (`send_to_id`) VALUES (:send_to_id);';
-		$stmt = $conn->prepare($sql);
-		$stmt->bindValue(':send_to_id', (int)$sendToID, PDO::PARAM_INT);
-		$stmt->execute();
-
-		$roomId = $conn->lastInsertId();
-		$sql = 'INSERT INTO messages (`text`, `send_from_id`, `room_id`) VALUES (:message, :send_from_id, :room_id);';
-		$stmt = $conn->prepare($sql);
-		$stmt->bindValue(':message', $message, PDO::PARAM_STR);
-		$stmt->bindValue(':send_from_id', (int)$_COOKIE['user_id'], PDO::PARAM_STR);
-		$stmt->bindValue(':room_id',(int)$roomId, PDO::PARAM_INT);
-		$stmt->execute();
-
-		return $roomId;
-	}
 
 ?>
